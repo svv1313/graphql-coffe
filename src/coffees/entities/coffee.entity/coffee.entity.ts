075a4c227e7668c676e1a4f39c38af11a7efa347
svv1313 <seleznev19963@gmail.com>
2024-08-05 16:40:02 +0200

    feat: connect DB to nest

--- src/coffees/entities/coffee.entity/coffee.entity.ts
+++ src/coffees/entities/coffee.entity/coffee.entity.ts
@@ -1,10 +1,19 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType({ description: 'The coffee model' })
 export class Coffee {
+  @PrimaryGeneratedColumn()
   @Field(() => ID, { description: 'Id of the coffee' })
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column({ type: 'json' })
   flavors: string[];
 }
