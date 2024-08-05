f6106095f181219f40a2407fecb9867add50e83c
svv1313 <seleznev19963@gmail.com>
2024-08-05 16:26:20 +0200

    feat: coffee service

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -2,23 +2,26 @@ import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Coffee } from './entities/coffee.entity/coffee.entity';
 import { ParseIntPipe } from '@nestjs/common';
 import { CreateCoffeeInput } from './dto/create-coffee.input/create-coffee.input';
+import { CoffeesService } from './coffees.service';
 
 @Resolver()
 export class CoffeesResolver {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Query(() => [Coffee], { name: 'coffees' })
   async findAll() {
-    return [];
+    return this.coffeesService.findAll();
   }
 
   @Query(() => Coffee, { name: 'coffee', nullable: true })
   async findOne(@Args('id', { type: () => ID }, ParseIntPipe) id: number) {
-    return null;
+    return this.coffeesService.findOne(id);
   }
 
   @Mutation(() => Coffee, { name: 'createCoffee', nullable: true })
   async create(
     @Args('createCoffeeInput') createCoffeeInput: CreateCoffeeInput,
   ) {
-    return null;
+    return this.coffeesService.create(createCoffeeInput);
   }
 }
